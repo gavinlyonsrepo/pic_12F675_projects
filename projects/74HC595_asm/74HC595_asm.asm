2af04c04dc4ee8ee27c92b9bc3b3deef129125ca
Gavin lyons <glyons66@hotmail.com>
2018-09-05 22:18:13 +0100

    adding 74HC595 project assembly

--- projects/74HC595_asm/74HC595_asm.asm
+++ projects/74HC595_asm/74HC595_asm.asm
@@ -1,4 +1,4 @@
-;
+; *  =========== HEADER ===========
 ; * File:   shift_asm.asm
 ; * Function: PIC12F675 8-bit binary counter, LED Array and  shift register
 ; * Author: Gavin Lyons
@@ -15,63 +15,58 @@
  __CONFIG _INTRC_OSC_NOCLKOUT & _WDT_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _CPD_OFF
 	
  
-;----------------------------------------------------------------------
-; RAM VARIABLES DEFINITIONS
-;----------------------------------------------------------------------
+
+; ========== RAM VARIABLES DEFINITIONS =================
+
     cblock 0x20
       COUNT1
       COUNT2
       COUNT3
-      BITS
-      BITS_COPY
+      DATA
+      DATA_COPY
     endc
     
- ;program  origin zero memory location
+ ;program origin zero memory location
    org 0
  
-;symbolic name section define pins connect to pic from 74HC595
-HC595_DS EQU 0  ; serial data in pin 14 
-HC595_ST_CP EQU 1 ; storage register clock input pin 12 "latch"  
-HC595_SH_CP EQU 2 ;shift register clock input pin 11 "storage"
+; symbolic name section define pins connect to pic from 74HC595
+SERIN_595 EQU 0  ; serial data in pin 14 
+LATCH_595 EQU 1 ; storage register clock input pin 12 "latch/RCLK""
+STORE_595 EQU 2 ;shift register clock input pin 11 "storage/SCLK"
 
-; Bank 0
-    bcf STATUS, RP0
+; ======== SETUP ==============
+    bcf STATUS, RP0 ; Bank 0
     clrf GPIO ; clear GPIO , low
-
-; Turns the comparator off, 
     movlw b'111'
-    movwf CMCON
+    movwf CMCON ; Turns the comparator off,
+    bsf STATUS, RP0 ; bank 1
+    clrf ANSEL	  ; All Analog pins are assigned as digital I/O
+    clrf TRISIO   ;  digital outputs,
+    bcf STATUS, RP0; Bank 0 .
 
-; bank 1
-    bsf STATUS, RP0
-    clrf ANSEL	; All Analog selections pins are assigned as digital I/O
-    clrf TRISIO   ; this turns all pins which can act as digital outputs,
-; Bank 0 .
-    bcf STATUS, RP0
-
-    
+; ========== MAIN LOOP ===================
 loop
     call Delay_1S
 
-; The HC595 shifts in from DS on SH_CP low-high transitions.
+; The HC595 shifts in from Serial in  on STORE_595 low-high transitions.
 SHIFT_CLOCK macro
-    bcf GPIO, HC595_SH_CP
-    bsf GPIO, HC595_SH_CP
+    bcf GPIO, STORE_595
+    bsf GPIO, STORE_595
     endm
 
-; Increment BITS and copy it.
-    incf BITS, F
-    movf BITS, W
-    movwf BITS_COPY
+; Increment DATA and copy it.
+    incf DATA, F
+    movf DATA, W
+    movwf DATA_COPY
 
-; Clock the least significant bit of BITS_COPY into the shift register, and
-; rotate bits,.
+; Clock the least significant bit of DATA_COPY into the shift register, and
+; rotate bits
 SHIFT_LSB_AND_CLOCK macro
-    bcf GPIO, HC595_DS
-    btfsc BITS_COPY, 0 ;skip if zero 
-    bsf GPIO, HC595_DS ;set serial high
+    bcf GPIO, SERIN_595
+    btfsc DATA_COPY, 0 ;skip if zero 
+    bsf GPIO, SERIN_595 ;set serial high
     SHIFT_CLOCK      ;set the clock
-    rrf BITS_COPY, F 
+    rrf DATA_COPY, F 
     endm
     
     ; bit 0 to 8 
@@ -85,19 +80,15 @@ SHIFT_LSB_AND_CLOCK macro
     SHIFT_LSB_AND_CLOCK
 
 ; Tick on the storage register clock(RCLK or latch) to copy the shift register in.
-    bcf GPIO, HC595_ST_CP
-    bsf GPIO, HC595_ST_CP
+    bcf GPIO, LATCH_595
+    bsf GPIO, LATCH_595
 
 ; Repeat this forever.
     goto loop
 
-;--------------------
-; SUB ROUNTINES
-;-------------------------------
+; ========= SUB ROUNTINES=========
 ; Generated cycle-accurate delay code using
-; http://www.golovchenko.org/cgi-bin/delay . Since the code was generated using
-; the assumption that the clock is exactly 4MHZ, this won't create a delay of
-; exactly one second, since the internal oscillator is not tuned that well.
+; http://www.golovchenko.org/cgi-bin/delay . 
 Delay_1S:
             ;999997 cycles
     movlw   0x08
