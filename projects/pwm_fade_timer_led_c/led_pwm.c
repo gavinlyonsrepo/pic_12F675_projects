f24ccd930af61e8b10b06054812e46bfaac3f6c1
Gavin lyons <glyons66@hotmail.com>
2019-01-25 23:17:47 +0000

    Tidy up code and comments

--- projects/pwm_fade_timer_led_c/led_pwm.c
+++ projects/pwm_fade_timer_led_c/led_pwm.c
@@ -1,99 +1,87 @@
 
 /*
- * File:   led_pwm.c
- * Author: gavin lyons
- *  Purpose  : Main file for PWM code for PIC12F675 using timer0.
- * IDE: MPLAB X v4.2 
- * Compiler: xc8 
+ * File: led_pwm.c
+ * Author: Gavin Lyons
+ * PIC: PIC12F675
+ * Purpose: File for LED fade using PWM code and timer0 see URL for more details
+ * IDE: MPLAB X v4.2
+ * Compiler: xc8
  * Created on 24 August 2018, 18:59
- * Url: https://github.com/gavinlyonsrepo/pic_12F675_projects
+ * URL: https://github.com/gavinlyonsrepo/pic_12F675_projects
  */
-
-/*
- 
-	   PIC TIMER0 Calculator
- 
-Clock Source in Mhz                   4 Mhz
-Fosc                                  4000000.0 Hz
-Fosc / 4                              1000000.0 Hz
-Time Period                           1e-06 sec
-Prescaler                             32
-Timer0 Interrupt Period               0.008192 sec
-Period of Frequency Input To Timer0   3.2e-05 sec
-Period of Time for each Timer0 Count  0.008192 sec
-1/((1000000/256)/32) ~= 8mS				
-*/
 
 #include <xc.h>
 
 // Config word
 __CONFIG(FOSC_INTRCIO & WDTE_OFF & PWRTE_ON & MCLRE_OFF & BOREN_OFF & CP_OFF & CPD_OFF);
 
-
 // Define CPU Frequency
-// This must be defined, if __delay_ms() or 
-// __delay_us() functions are used in the code
-#define _XTAL_FREQ   4000000    
+#define _XTAL_FREQ   4000000
 // Define PWM pin
 #define PWM_Pin    GP4
 #define usdelay  10
-// Define PWM variable, It can have a value 
+// Define PWM variable, It can have a value
 // from 0 (0% duty cycle) to 255 (100% duty cycle)
 unsigned char PWM = 255;
 
-
-void interrupt ISR(void)
+// function to setup PIC
+void setup(void)
 {
-	if(T0IF)  //If Timer0 Interrupt
-	{
-		if(PWM_Pin)	// if PWM_Pin is high
-		{
-			PWM_Pin = 0;
-			TMR0 = PWM;
-		}
-		else	     // if PWM_Pin is low
-		{
-			PWM_Pin = 1;
-			TMR0 = 255 - PWM;
-		}
-
-		T0IF = 0;   // Clear the interrupt
-	}
+    ANSEL  = 0x00;       // Set ports as digital I/O, not analog input
+    ADCON0 = 0x00;       // Shut off the A/D Converter
+    CMCON  = 0x07;       // Shut off the Comparator
+    VRCON  = 0x00;       // Shut off the Voltage Reference
+    TRISIO = 0x00;       // All GPIO output
+    GPIO   = 0x00;       // Make all pins low
+    WPU    = 0;          // Disable all weak pull up
+    InitPWM();           // Initialize PWM
 }
 
+// Function to handle interrupt on timer0 overflow.
+void interrupt ISR(void)
+{
+    if(T0IF)  //If Timer0 Interrupt
+    {
+        if(PWM_Pin) // if PWM_Pin is high
+        {
+            PWM_Pin = 0;
+            TMR0 = PWM;
+        }
+        else         // if PWM_Pin is low
+        {
+            PWM_Pin = 1;
+            TMR0 = 255 - PWM;
+        }
+        T0IF = 0;   // Clear the interrupt
+    }
+}
 
+//Function to init Timer0
 void InitPWM(void)
 {
-	// Use timer0 for making PWM
-	OPTION_REG &= 0xCC;     // Intialise timer0
-	//INTCON     = 0b10100000;     // Global Interrupt Enabled and TMR0 Overflow Interrupt Enabled
-	TMR0       = 0         ;     // Preload timer register
-	T0IE = 1;				// Enable Timer0 interrupt
-	GIE = 1;				// Enable global interrupts
+    // Use timer0 for making PWM
+    OPTION_REG &= 0xCC;     // Intialise timer0
+    TMR0 = 0;         // Preload timer register
+    T0IE = 1;               // Enable Timer0 interrupt
+    GIE = 1;                // Enable global interrupts
 }
 
 // Main function
 void main()
-{	
-	ANSEL  = 0x00;       // Set ports as digital I/O, not analog input
-	ADCON0 = 0x00;		 // Shut off the A/D Converter
-	CMCON  = 0x07;		 // Shut off the Comparator
-	VRCON  = 0x00;	     // Shut off the Voltage Reference
-	TRISIO = 0x00;       // All output
-	GPIO   = 0x00;       // Make all pins 0
-	WPU    = 0;              // Disable all weak pull up
-	InitPWM();			 // Initialize PWM
-
-	// PWM=0 means 0% duty cycle and 
-	// PWM=255 means 100% duty cycle
-	//PWM = 127;			 // 50% duty cycle 
-	int d = 1;
-	// cycle PWM from 255 to 0 to 255 to etc
-	while (1) {
-	PWM += d;
-	if(PWM == 255) d = -1;
-	__delay_ms(usdelay);
-	if(PWM ==   0) d = 1;
+{
+    setup();
+    int d = 1;
+    
+    // cycle PWM from 255 to 0 to 255 to etc
+    // PWM=0 means 0% duty cycle 
+    //PWM = 127 means 50% duty cycle
+    // PWM=255 means 100% duty cycle
+    while (1) {
+        PWM += d;
+        if(PWM == 255) d = -1;
+        __delay_ms(usdelay);
+        if(PWM ==   0) d = 1;
   }
-	
+
 }
+//  EOF
